0590f32457171255fc1210713864f9bf019e467c
adriancgarcia <adriancgarcia@gmail.com>
2024-07-04 00:46:31 -0500

    removed bottom borer so no scrolling on mobile home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -41,7 +41,7 @@ const Home = () => {
                         </Link> 
                     </motion.div>
                     {/* image */} 
-                    <div className='flex justify-center xs:max-h-80 lg: h-full max-h-[650px]'>
+                    <div className='flex justify-center xs:max-h- lg: h-full max-h-[650px]'>
                         <motion.div 
                             initial={{ scale: 0 }}
                             animate={{ scale: 1 }}
