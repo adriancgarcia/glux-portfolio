87f429d2d348408aef4b5119bc4a763dde7fd323
adriancgarcia <adriancgarcia@gmail.com>
2024-07-04 01:06:43 -0500

    trying to get page to scroll under header.

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,8 +11,8 @@ import { Link } from 'react-router-dom';
 const Header =() => {
     // const { mouseEnterHandler, mouseLeaveHandler } = useContext(CursorContext);
     return (
-        <header className='fixed w-full px-[30px] lg:px-[100] z-30 h-[100px] lg:h-[140px] flex items-center pt-6'>
-            <div className="flex flex-col lg:flex-row  lg:items-center w-full justify-between pl-20 xs: scroll-pl-20">
+        <header className='fixed w-full px-[30px] lg:px-[100] z-30 h-[100px] lg:h-[140px] flex items-center pt-6 h-ecreen overflow-hidden'>
+            <div className="flex flex-col flex-1 overflow-y-scroll lg:flex-row lg:items-center w-full justify-between pl-20 xs: scroll-pl-20">
                 {/* logo */}
                 <Link 
                     // onMouseEnter={mouseEnterHandler}
