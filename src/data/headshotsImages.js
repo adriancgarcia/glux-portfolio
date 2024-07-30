b06ace79c0300bdc94bbc2ad2f3807e2ef6b1ad0
adriancgarcia <adriancgarcia@gmail.com>
2024-07-30 16:23:57 -0500

    added images to headshots page.

--- src/data/headshotsImages.js
+++ src/data/headshotsImages.js
@@ -1,78 +1,88 @@
 export const headshotsImages = [
     {
         src: '../img/headshots/Nina2.jpg',
-        title: 'Image2',
+        title: 'Image1',
         description: '',
     },
 
     {
         src: '../img/headshots/Nina3.jpg',
-        title: 'Image3',
+        title: 'Image2',
         description: '',
     },
     {
         src: '../img/headshots/Nina4.jpg',
-        title: 'Image4',
+        title: 'Image3',
         description: '',
     },
     {
         src: '../img/headshots/Nina5.JPG',
-        title: 'Image5',
+        title: 'Image4',
         description: '',
     },
     {
         src: '../img/headshots/Nina6.JPG',
+        title: 'Image5',
+        description: '',
+    },
+    {
+        src: '../img/headshots/ACG_David_Lozano_0001.jpg',
         title: 'Image6',
+        description: '',
+    },
+    {
+        src: '../img/headshots/ACG_David_Lozano_0015.jpg',
+        title: 'Image7',
         description: '',
     },
     {
         src: '../img/headshots/ACG_EC122.jpg',
-        title: 'Image6',
+        title: 'Image8',
         description: '',
     },
     {
         src: '../img/headshots/ACG_EC128.jpg',
-        title: 'Image6',
+        title: 'Image9',
         description: '',
     },
     {
         src: '../img/headshots/ACG_EC132.jpg',
-        title: 'Image6',
+        title: 'Image10',
         description: '',
     },
     {
         src: '../img/headshots/ACG_EC145.jpg',
-        title: 'Image6',
+        title: 'Image11',
         description: '',
     },
     {
         src: '../img/headshots/ACG_EC158.jpg',
-        title: 'Image6',
+        title: 'Image12',
         description: '',
     },
     {
         src: '../img/headshots/ACG_EC161.jpg',
-        title: 'Image6',
+        title: 'Image12',
         description: '',
     },
     {
         src: '../img/headshots/ACG_EC166.jpg',
-        title: 'Image6',
+        title: 'Image13',
         description: '',
     },
     {
         src: '../img/headshots/ACG_EC175.jpg',
-        title: 'Image6',
+        title: 'Image14',
         description: '',
     },
     {
         src: '../img/headshots/ACG_EC185.jpg',
-        title: 'Image6',
+        title: 'Image15',
         description: '',
     },
     {
         src: '../img/headshots/ACG_EC186.jpg',
-        title: 'Image6',
+        title: 'Image16',
         description: '',
     },
 ];
