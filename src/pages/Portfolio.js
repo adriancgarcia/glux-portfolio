51f3608901304bfda5efb8629cc3113601f115bc
adriancgarcia <adriancgarcia@gmail.com>
2024-09-11 17:57:23 -0500

    trying to fix portfoilio image for headshots. Currently too big

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -82,7 +82,7 @@ const Portfolio =() => {
                         <div className='max-w-[250px] lg:max-w-[320px] h-[187px] lg:[220px] bg-accent overflow-hidden flex justify-center items-center'>
                             <Link to={'/headshots'}>
                                     <h2>Headshots</h2>
-                                <img className='object-cover h-full lg:h-[165px] hover:scale-110 transition-all duration-500' src={Image5} alt=''/>
+                                <img className='object-cover sm: h-[165px] hover:scale-110 transition-all duration-500' src={Image5} alt=''/>
                             </Link >    
                         </div>
                         <div className='max-w-[250px] lg:max-w-[320px] h-[187px] lg:[220px] bg-accent overflow-hidden'>
