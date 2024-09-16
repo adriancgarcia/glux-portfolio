7952fbec3eefa0dce159ea63ed331ba971ad8a59
adriancgarcia <adriancgarcia@gmail.com>
2024-09-16 14:36:44 -0500

    added images to events page

--- src/data/eventImages.js
+++ src/data/eventImages.js
@@ -1,73 +1,306 @@
 export const eventImages = [
     {
-        src: '../img/events/ACG_0025.jpg',
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0001.jpg',
         title: 'Image1',
         description: '',
     },
     {
-        src: '../img/events/ACG_0032.jpg',
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0002.jpg',
         title: 'Image2',
         description: '',
     },
 
     {
-        src: '../img/events/ACG_0080.jpg',
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0003.jpg',
         title: 'Image3',
         description: '',
     },
     {
-        src: '../img/events/The_Rookery.jpg',
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0004.jpg',
         title: 'Image4',
         description: '',
     },
     {
-        src: '../img/events/Navy_Pier2.JPG',
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0005.JPG',
         title: 'Image5',
         description: '',
     },
     {
-        src: '../img/events/Navy_Pier1.JPG',
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0006.JPG',
         title: 'Image6',
         description: '',
     },
     {
-        src: '../img/events/image-1.jpg',
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0007.jpg',
         title: 'Image7',
         description: '',
     },
     {
-        src: '../img/events/image-2.jpg',
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0008.jpg',
         title: 'Image8',
         description: '',
     },
     {
-        src: '../img/events/image-3.jpg',
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0009.jpg',
         title: 'Image9',
         description: '',
     },
     {
-        src: '../img/events/image-4.jpg',
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0010.jpg',
         title: 'Image10',
         description: '',
     },
     {
-        src: '../img/events/image-5.jpg',
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0011.jpg',
         title: 'Image11',
         description: '',
     },
     {
-        src: '../img/events/image-6.jpg',
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0012.jpg',
         title: 'Image12',
         description: '',
     },
     {
-        src: '../img/events/image-7.jpg',
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0013.jpg',
         title: 'Image13',
         description: '',
     },
     {
-        src: '../img/events/image-8.jpg',
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0014.jpg',
         title: 'Image14',
         description: '',
     },
+    {
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0015.jpg',
+        title: 'Image15',
+        description: '',
+    },
+
+    {
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0016.jpg',
+        title: 'Image16',
+        description: '',
+    },
+    {
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0017.jpg',
+        title: 'Image17',
+        description: '',
+    },
+    {
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0018.jpg',
+        title: 'Image18',
+        description: '',
+    },
+    {
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0019.jpg',
+        title: 'Image19',
+        description: '',
+    },
+    {
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0020.jpg',
+        title: 'Image20',
+        description: '',
+    },
+    {
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0021.jpg',
+        title: 'Image21',
+        description: '',
+    },
+    {
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0022.jpg',
+        title: 'Image22',
+        description: '',
+    },
+    {
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0023.jpg',
+        title: 'Image23',
+        description: '',
+    },
+    {
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0024.jpg',
+        title: 'Image24',
+        description: '',
+    },
+    {
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0025.jpg',
+        title: 'Image25',
+        description: '',
+    },
+    {
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0026.jpg',
+        title: 'Image26',
+        description: '',
+    },
+    {
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0027.jpg',
+        title: 'Image27',
+        description: '',
+    },
+    {
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0028.jpg',
+        title: 'Image28',
+        description: '',
+    },
+    {
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0029.jpg',
+        title: 'Image29',
+        description: '',
+    },
+    {
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0030.jpg',
+        title: 'Image30',
+        description: '',
+    },
+    {
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0031.jpg',
+        title: 'Image31',
+        description: '',
+    },
+    {
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0032.jpg',
+        title: 'Image32',
+        description: '',
+    },
+    {
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0033.jpg',
+        title: 'Image33',
+        description: '',
+    },
+    {
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0034.jpg',
+        title: 'Image34',
+        description: '',
+    },
+    {
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0035.jpg',
+        title: 'Image35',
+        description: '',
+    },
+    {
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0036.jpg',
+        title: 'Image36',
+        description: '',
+    },
+    {
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0037.jpg',
+        title: 'Image37',
+        description: '',
+    },
+    {
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0038.jpg',
+        title: 'Image38',
+        description: '',
+    },
+    {
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0039.jpg',
+        title: 'Image39',
+        description: '',
+    },
+    {
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0040.jpg',
+        title: 'Image40',
+        description: '',
+    },
+    {
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0041.jpg',
+        title: 'Image41',
+        description: '',
+    },
+    {
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0042.jpg',
+        title: 'Image42',
+        description: '',
+    },
+    {
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0043.jpg',
+        title: 'Image43',
+        description: '',
+    },
+    {
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0044.jpg',
+        title: 'Image44',
+        description: '',
+    },
+    {
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0045.jpg',
+        title: 'Image45',
+        description: '',
+    },
+    {
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0046.jpg',
+        title: 'Image46',
+        description: '',
+    },
+    {
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0047.jpg',
+        title: 'Image47',
+        description: '',
+    },
+    {
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0048.jpg',
+        title: 'Image48',
+        description: '',
+    },
+    {
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0049.jpg',
+        title: 'Image49',
+        description: '',
+    },
+    {
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0050.jpg',
+        title: 'Image50',
+        description: '',
+    },
+    {
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0051.jpg',
+        title: 'Image51',
+        description: '',
+    },
+    {
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0052.jpg',
+        title: 'Image52',
+        description: '',
+    },
+    {
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0053.jpg',
+        title: 'Image53',
+        description: '',
+    },
+    {
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0054.jpg',
+        title: 'Image54',
+        description: '',
+    },
+    {
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0055.jpg',
+        title: 'Image55',
+        description: '',
+    },
+    {
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0056.jpg',
+        title: 'Image56',
+        description: '',
+    },
+    {
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0057.jpg',
+        title: 'Image57',
+        description: '',
+    },
+    {
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0058.jpg',
+        title: 'Image58',
+        description: '',
+    },
+    {
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0059.jpg',
+        title: 'Image59',
+        description: '',
+    },
+    {
+        src: '../img/events/07022018_ACG_JonLester_NVRQT_Fundraiser_0060.jpg',
+        title: 'Image60',
+        description: '',
+    },
+   
+
 ];
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Image1 from '../img/portfolio/wedding1.jpg';
 import Image2 from '../img/portfolio/_ACG1319.jpg';
 import Image3 from '../img/portfolio/Candies_MaternityShoot_Morgan_0322.jpg';
-// import Image4 from '../img/portfolio/Navy_Pier1.JPG';
+import Image4 from '../img/portfolio/07022018_ACG_JonLester_NVRQT_Fundraiser_0009.jpg';
 import Nina3 from '../img/portfolio/Nina3.jpg';
 import Image5 from '../img/portfolio/Cindy.jpg';
 
@@ -72,12 +72,12 @@ const Portfolio =() => {
                             </Link >    
                         </div>
 
-                        {/* <div className='max-w-[250px] lg:max-w-[320px] h-[187px] lg:[220px] bg-accent overflow-hidden'>
+                        <div className='max-w-[250px] lg:max-w-[320px] h-[187px] lg:[220px] bg-accent overflow-hidden'>
                             <Link to={'/events'}>
                                     <h2>Events</h2>
                                 <img className='object-cover h-full lg:h-[220px] hover:scale-110 transition-all duration-500' src={Image4} alt=''/>
                             </Link >    
-                        </div> */}
+                        </div>
 
                         <div className='max-w-[250px] lg:max-w-[320px] h-[187px] lg:[220px] bg-accent overflow-hidden flex justify-center items-center'>
                             <Link to={'/headshots'}>
